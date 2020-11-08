e8e57c9f4f9d2e30061021b77db26a4761bab897
Devansh Bindlish <devanshbindlish999@gmail.com>
2020-11-08 16:15:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    const navSlide = function(){
+        var burgerr = document.querySelector(".burger");
+        const nav = document.querySelector(".nav-links");
+        const navLinks = document.querySelectorAll(".nav-links li");
+
+        burgerr.addEventListener("click",function(){
+            nav.classList.toggle("nav-active");
+            // console.log("clicked")
+            //link animation
+        navLinks.forEach((link,index)=>{
+            if(link.style.animation){
+                link.style.animation = ``;
+            }
+            else
+            link.style.animation = `navLinkFade 0.5s ease forwards ${(index/7) + 0.65}s`        //inndex/5 s for delay
+        });
+        burgerr.classList.toggle("toggle");
+        });
+        
+    };
+    navSlide();
+    //classList error
+};
